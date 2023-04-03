df7de8a368d742642df340b2f4d8f9f37d95e6a5
Wahid Hidayat <wahid.hidayat@samudrainovasi.co.id>
2023-04-03 15:10:04 +0700

    first commit

---
+++ SlideMath/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SlideMath
+//
+//  Created by Wahid Hidayat on 03/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlideMath/SlideMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMathApp.swift
+//  SlideMath
+//
+//  Created by Wahid Hidayat on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SlideMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
