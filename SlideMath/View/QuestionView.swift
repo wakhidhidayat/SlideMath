43fc0abea8e6d06dc074f3293f12c3e486bc9582
agfidprasetyo <agfid11@gmail.com>
2023-04-12 20:59:23 +0700

    Fix Conflict Question View

--- SlideMath/View/QuestionView.swift
+++ SlideMath/View/QuestionView.swift
@@ -16,108 +16,119 @@ enum Position {
 struct QuestionView: View {
     
     var question: [String] = ["Sebuah kapal selam menyelam hingga ke kedalaman 40 meter di bawah permukaan laut.", "Kemudian, kapal selam tersebut kembali menyelam sejauh 60 meter. Maka posisi kapal selam sekarang berada pada kedalaman?"]
+    @State private var isNavigationActive = false
     @State var currentQuestion: Int = 0
     @State var value = 0.0
-    
+    @State var valueSlider = 0.0
+
     @State var showSpotLight: Bool = false
     @State var currentSpot: Int = 0
     @State var position: Position = .top
     
     var body: some View {
-        ZStack() {
-            VStack() {
-                Spacer()
-                Image("background_question").resizable().scaledToFit()
-            }
-            ZStack {
-                VStack(spacing: 50) {
-                    HStack(alignment: .top) {
-                        Button {
-                            print("back")
-                        } label: {
-                            Image("back_button")
-                        }
-                        Spacer()
-                        Button {
-                            print("help")
-                        } label: {
-                            Image("help_button")
-                        }
-                        .addSpotlight(0, shape: .rounded, roundedRadius: 10, text: """
-For walkthrough
-""", position: .top)
+        NavigationStack {
+            ZStack() {
+                VStack() {
+                    Spacer()
+                    Image("background_question").resizable().scaledToFit()
+                }
+                ZStack {
+                    VStack(spacing: 50) {
+                        HStack(alignment: .top) {
+                            Button {
+                                print("back")
+                            } label: {
+                                Image("back_button")
+                            }
+                            Spacer()
+                            Button {
+                                print("help")
+                            } label: {
+                                Image("help_button")
+                            }
+                            .addSpotlight(0, shape: .rounded, roundedRadius: 10, text: """
+    For walkthrough
+    """, position: .top)
 
-                            
-                    }
-                    
-                    ProgressView(value: Double(currentQuestion+1), total: Double(question.count))
-                        .tint(Color.blue)
-                        .listRowSeparator(.visible)
-                        .listRowSeparatorTint(Color.blue)
-                        .listSectionSeparator(.visible)
-                        .listSectionSeparatorTint(Color.blue)
-                        .background(Color(.white))
-                        .frame(width: 300, height: 23)
-                        .scaleEffect(x: 1, y: 24, anchor: .center)
-                        .cornerRadius(64)
-                    Text(question[currentQuestion]).font(.title2).fontWeight(.semibold).foregroundColor(Color("green_text")).multilineTextAlignment(.center)
-                        .fixedSize(horizontal: false, vertical: true)
-                    Spacer()
-                }
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                VStack {
-                    Spacer()
-                    Spacer()
-                    HStack(alignment: .center, spacing: 30) {
-                        Button(action: {
-                            currentQuestion = currentQuestion != 0 ? currentQuestion - 1 : currentQuestion
-                        }) {
-                            Image(currentQuestion == 0 ? "back_quest_inactive" : "back_quest_active")
+                                
                         }
-                        Button(action: {
-                            currentQuestion = currentQuestion == question.count-1 ? currentQuestion : currentQuestion + 1
-                        }) {
-                            Image(currentQuestion == question.count-1 ? "next_quest_inactive" : "next_quest_active")
-                        }
-                        .addSpotlight(2, shape: .circle, roundedRadius: 30, text: """
-Next Question
-""", position: .top)
                         
+                        ProgressView(value: Double(currentQuestion+1), total: Double(question.count))
+                            .tint(Color.blue)
+                            .listRowSeparator(.visible)
+                            .listRowSeparatorTint(Color.blue)
+                            .listSectionSeparator(.visible)
+                            .listSectionSeparatorTint(Color.blue)
+                            .background(Color(.white))
+                            .frame(width: 300, height: 23)
+                            .scaleEffect(x: 1, y: 24, anchor: .center)
+                            .cornerRadius(64)
+                        Text(question[currentQuestion]).font(.title2).fontWeight(.semibold).foregroundColor(Color("green_text")).multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                        Spacer()
                     }
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                }
-                
-                VStack(spacing: 20) {
-                    Spacer()
-                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    HStack{
-                        Button {
-                            print("back")
-                        } label: {
-                            Image("hint_button")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        HStack(alignment: .center, spacing: 30) {
+                            Button(action: {
+                                currentQuestion = currentQuestion != 0 ? currentQuestion - 1 : currentQuestion
+                            }) {
+                                Image(currentQuestion == 0 ? "back_quest_inactive" : "back_quest_active")
+                            }
+                            Button(action: {
+                                currentQuestion = currentQuestion == question.count-1 ? currentQuestion : currentQuestion + 1
+                            }) {
+                                Image(currentQuestion == question.count-1 ? "next_quest_inactive" : "next_quest_active")
+                            }
+                            .addSpotlight(2, shape: .circle, roundedRadius: 30, text: """
+    Next Question
+    """, position: .top)
+                            
                         }
-                        .padding(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                        .addSpotlight(1, shape: .circle, roundedRadius: 50, text: """
-Hint
-""", position: .bottom)
+                        Spacer()
+                        Spacer()
                         Spacer()
                     }
                     
-                    
-                    PrimaryButton(title: "Submit", isDisabled: currentQuestion == 0) {
-                        // TODO: Submit
+                    CustomSlider(value: $valueSlider, range: (0, 100))
+
+                    VStack(spacing: 20) {
+                        Spacer()
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        HStack{
+                            Button {
+                                print("back")
+                            } label: {
+                                Image("hint_button")
+                            }
+                            .padding(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                            .addSpotlight(1, shape: .circle, roundedRadius: 50, text: """
+    Hint
+    """, position: .bottom)
+                            Spacer()
+                        }
+                        
+                        
+                        PrimaryButton(title: "Submit", isDisabled: currentQuestion != question.count - 1) {
+                            isNavigationActive = true
+                            print("isNavigation: ", isNavigationActive)
+                        }
+
+                        Spacer()
+                        Spacer()
                     }
-                    Spacer()
-                    Spacer()
                 }
+            }.navigationDestination(isPresented: $isNavigationActive) {
+                ResultView(result: valueSlider == Double(-90) ? .correct : .incorrect).navigationBarBackButtonHidden(true)
             }
-        }.ignoresSafeArea()
-            .addSpotLightOverlay(show: $showSpotLight, currentSpot: $currentSpot, position: .bottom)
-            .onAppear {
-                showSpotLight = true
-            }
+            .ignoresSafeArea()
+                .addSpotLightOverlay(show: $showSpotLight, currentSpot: $currentSpot, position: .bottom)
+                .onAppear {
+                    showSpotLight = true
+                }
+        }
     }
 }
 
